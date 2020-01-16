d6f06d585b03396b46082ba5d10d04b56f1cbde9
Gary Clark <gclark04432@live.co.uk>
2020-01-16 11:16:00 +0000

    can get data from api

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      beers: [],
+    }
+  },
+  mounted() {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(result => result.json())
+      .then(beers => this.beers = beers)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
